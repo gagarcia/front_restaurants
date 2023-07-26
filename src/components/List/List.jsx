773b5a67f5069e90ba8d4d57aedac6a9be8c946b
Gabriel Nogueira Garcia <gagarcia@users.noreply.github.com>
2023-07-25 21:33:18 -0300

    first commit

---
+++ src/components/List/List.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import useStyles from './styles';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+
+const List = () => {
+    const classes = useStyles();
+    const [type, setType] = useState('restaurants')
+    const [rating, setRating] = useState('')
+    const places = [
+        {
+            'name': 'Cerveja' 
+        },
+        {
+            'name': 'Steak'
+        },
+        {
+            'name': 'Cerveja' 
+        },
+        {
+            'name': 'Steak'
+        },
+    ]
+
+
+    return (
+        <div className={classes.container}>
+            <Typography variant="h4">Restaurantes perto de você</Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value='restaurants'>Restaurantes</MenuItem>
+                    <MenuItem value='bars'>Bares</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3</MenuItem>
+                    <MenuItem value={4}>Above 4</MenuItem>
+                </Select>
+            </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, i) => (
+                    <Grid item key={i} xs={12}>
+                        <PlaceDetails place={place} />
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    )
+}
+
+export default List 
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles.js';
+
+const apiKey = 'AIzaSyBXedMiFX16twofsbJev4SvuxjMWmGEmTs'
+
+const Map = () => {
+
+    const matches = useMediaQuery('(min-width:600px)');
+    const classes = useStyles();
+
+    const coordinates = {
+        'lat': -21.19137,
+        'lng': -47.78118
+    };
+
+    return (
+        <div className={ classes.mapContainer }>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: apiKey }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+
+        </div>
+    )
+}
+
+export default Map 
